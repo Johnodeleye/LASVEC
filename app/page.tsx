7e96dfea4d2725a32a6e5ede6e787e3108c390fa
Perspicacious <johnayomide920@gmail.com>
2024-11-24 16:34:29 +0100

    day 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About"
 import Features from "@/components/Features"
 import Grid from "@/components/Grid"
+import Why from "@/components/Why"
 
 const page = () => {
   return (
@@ -8,6 +9,7 @@ const page = () => {
       <Grid/>
       <Features/>
       <About/>
+      <Why/>
     </div>
   )
 }
--- components/About.tsx
+++ components/About.tsx
@@ -1,24 +1,52 @@
-import { div } from "framer-motion/client"
+'use client';
 import { Facebook, Github, Instagram, Linkedin, Twitter, Youtube } from "lucide-react"
+import { motion } from "motion/react"
+import LinkedIn from "next-auth/providers/linkedin"
 
 const About  = () => {
     return (
         <div id="About">
 <section className="">
-  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
-      <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+  <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1.5 }}
+       className="py-8 px-11 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+      <motion.div
+      initial={{ opacity: 0, y:100 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }} 
+      whileInView={{ opacity: 1, y:0 }}
+      viewport={{once: true}}
+       className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-blue-600 text-left md:text-center">ABOUT US</h2>
           <h3 className="mb-4 text-2xl tracking-tight  text-blue-600 text-left"> <span className="font-bold">LASVEC</span> is Lagos State's Official Electronic Voting System, used to conduct credible and transparent elections from anywhere, at any time.</h3>
           <p className="font-light text-gray-500 lg:mb-16 sm:text-xl text-left">We aim to revolutionize the electoral process in Lagos State with LASVEC, a cutting-edge e-voting platform designed to make elections more accessible, secure, and transparent.</p>
-      </div> 
-      <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+      </motion.div> 
+      <motion.div
+      initial={{ opacity: 0, y:100 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }} 
+      whileInView={{ opacity: 1, y:0 }}
+      viewport={{once: true}} className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
           <h4 className="mb-4 text-4xl tracking-tight text-blue-900 mt-16 lg:mt-24">Management Team</h4>
-      </div> 
-      <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+      </motion.div> 
+      <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1.5 }} 
+      className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
         {/* Man 1 */}
-          <div className="items-center bg-gray-100 rounded-lg shadow sm:flex transition-all duration-500 cursor-pointer hover:bg-blue-600 group">
-    <a href="#">
-        <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Avatar"/>
+        <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.1 }} 
+  className="items-center bg-gray-100 rounded-lg shadow sm:flex transition-all duration-500 cursor-pointer hover:bg-blue-600 group">
+        <a href="https://perspicaciousdev.vercel.app">
+        <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="/John.webp" alt="Bonnie Avatar"/>
     </a>
         <div className="p-5">
         <h3 className="text-xl font-bold tracking-tight text-gray-900 group-hover:text-white">
@@ -39,7 +67,7 @@ const About  = () => {
                         </li>
                         <li>
                             <a href="#" className="text-gray-500  hover:text-white group-hover:text-white">
-                               <Github className="w-4/5"/>
+                               <Linkedin className="w-4/5"/>
                             </a>
                         </li>
                         <li>
@@ -49,19 +77,23 @@ const About  = () => {
                         </li>
                     </ul>
                 </div>
-            </div> 
+            </motion.div> 
 
             {/* Man 2 */}
-            <div className="items-center bg-gray-100 rounded-lg shadow sm:flex transition-all duration-500 cursor-pointer hover:bg-blue-600 group">
+                 <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.1 }}  className="items-center bg-gray-100 rounded-lg shadow sm:flex transition-all duration-500 cursor-pointer hover:bg-blue-600 group">
     <a href="#">
-        <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Avatar"/>
+        <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_episode400/2164677/2164677-1661647446915-d2fe2f1a19451.jpg" alt="akintunde Avatar"/>
     </a>
   <div className="p-5">
     <h3 className="text-xl font-bold tracking-tight text-gray-900 group-hover:text-white">
-      <a href="#" className="group-hover:text-white">Jese Leos</a>
+      <a href="https://instagram.com/akintunde_brave" className="group-hover:text-white">AKINTUNDE OPEYEMI IDOWU</a>
     </h3>
-    <span className="text-gray-500 group-hover:text-white">CTO</span>
-    <p className="mt-3 mb-4 font-light text-gray-500 group-hover:text-white">Jese drives the technical strategy of the flowbite platform and brand.</p>
+    <span className="text-gray-500 group-hover:text-white">Host, All Things Teens Podcast</span>
+    <p className="mt-3 mb-4 font-light text-gray-500 group-hover:text-white">Akintunde is the driving force behind @allthingsteenspodcast, a platform where he shares inspiring stories, insightful discussions, and expert advice, catering to the interests and concerns of teenagers and young adults.</p>
     <ul className="flex space-x-4 sm:mt-0">
       <li>
         <a href="#" className="text-gray-500 hover:text-white group-hover:text-white">
@@ -85,19 +117,24 @@ const About  = () => {
       </li>
     </ul>
   </div>
-            </div> 
+            </motion.div> 
 
             {/* Man 3 */}
-            <div className="items-center bg-gray-100 rounded-lg shadow sm:flex transition-all duration-500 cursor-pointer hover:bg-blue-600 group">
+            <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.1 }} 
+       className="items-center bg-gray-100 rounded-lg shadow sm:flex transition-all duration-500 cursor-pointer hover:bg-blue-600 group">
     <a href="#">
-        <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Avatar"/>
+        <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://d3t3ozftmdmh3i.cloudfront.net/staging/podcast_uploaded_nologo400/2164677/2164677-1721211901946-bbc24b23d861b.jpg" alt="Jese Avatar"/>
     </a>
   <div className="p-5">
     <h3 className="text-xl font-bold tracking-tight text-gray-900 group-hover:text-white">
-      <a href="#" className="group-hover:text-white">Jese Leos</a>
+      <a href="#" className="group-hover:text-white">ALL THINGS TEENS PODCAST</a>
     </h3>
-    <span className="text-gray-500 group-hover:text-white">CTO</span>
-    <p className="mt-3 mb-4 font-light text-gray-500 group-hover:text-white">Jese drives the technical strategy of the flowbite platform and brand.</p>
+    <span className="text-gray-500 group-hover:text-white">"Host & Organizer, All Things Teens Podcast and Annual Teens Summit</span>
+    <p className="mt-3 mb-4 font-light text-gray-500 group-hover:text-white">All Things Teens Podcast delves into real conversations and stories that matter to teens.</p>
     <ul className="flex space-x-4 sm:mt-0">
       <li>
         <a href="#" className="text-gray-500 hover:text-white group-hover:text-white">
@@ -121,19 +158,23 @@ const About  = () => {
       </li>
     </ul>
   </div>
-            </div> 
+            </motion.div> 
 
             {/* Man 4 */}
-            <div className="items-center bg-gray-100 rounded-lg shadow sm:flex transition-all duration-500 cursor-pointer hover:bg-blue-600 group">
+        <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.1 }}  className="items-center bg-gray-100 rounded-lg shadow sm:flex transition-all duration-500 cursor-pointer hover:bg-blue-600 group">
     <a href="#">
-        <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Avatar"/>
+        <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3f2_JAi-Bkya-51co_g67oeNL3NvcM7ya_Q&s" alt="Inec Avatar"/>
     </a>
   <div className="p-5">
     <h3 className="text-xl font-bold tracking-tight text-gray-900 group-hover:text-white">
-      <a href="#" className="group-hover:text-white">Jese Leos</a>
+      <a href="#" className="group-hover:text-white">INEC</a>
     </h3>
-    <span className="text-gray-500 group-hover:text-white">CTO</span>
-    <p className="mt-3 mb-4 font-light text-gray-500 group-hover:text-white">Jese drives the technical strategy of the flowbite platform and brand.</p>
+    <span className="text-gray-500 group-hover:text-white"> Host & Organizer of Elections in Nigeria.</span>
+    <p className="mt-3 mb-4 font-light text-gray-500 group-hover:text-white">INEC has established physical structures and systems to promote democratic governance and stability in Nigeria through effective electoral management.</p>
     <ul className="flex space-x-4 sm:mt-0">
       <li>
         <a href="#" className="text-gray-500 hover:text-white group-hover:text-white">
@@ -157,9 +198,9 @@ const About  = () => {
       </li>
     </ul>
   </div>
-            </div> 
-      </div>  
-  </div>
+            </motion.div> 
+      </motion.div>  
+  </motion.div>
 </section>
         </div>
     )
